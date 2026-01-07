71ad786091da9fd096927505ab8e44fd04afe82c
antonyrahul785-prog <antonyrahul785@gmail.com>
2026-01-07 20:05:07 +0530

    theo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './App.css'; // or './App.css'
 
 // Create the root for React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
